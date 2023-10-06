5bbc651fa4f4a751fc2b53a168ac2704946b2027
Khalanskyi Oleh <112695863+OKhalanskyi@users.noreply.github.com>
2023-10-06 23:05:34 +0300

    feat: input as uikit added

---
+++ src/shared/ui-kit/input/Input.tsx
@@ -0,0 +1,62 @@
+import { ChangeEvent, FC, InputHTMLAttributes, memo, useCallback, useEffect, useRef, useState } from 'react';
+import * as classNames from 'classnames';
+
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+
+interface InputProps extends HTMLInputProps {
+  className?: string;
+  value?: string | number;
+  onChange?: (value: string) => void;
+}
+
+const Input: FC<InputProps> = memo((props) => {
+  const {
+    className,
+    value,
+    onChange,
+    placeholder,
+    type = 'text',
+    autoFocus,
+    ...otherProps
+  } = props;
+
+  const [isFocused, setIsFocused] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (autoFocus) {
+      setIsFocused(true);
+      inputRef.current?.focus();
+    }
+  }, [autoFocus]);
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange?.(e.target.value);
+  };
+  const onBlur = useCallback(() => {
+    setIsFocused(false);
+  }, []);
+
+  const onFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  return (
+    <input
+      ref={inputRef}
+      value={value}
+      type={type}
+      onChange={onChangeHandler}
+      placeholder={placeholder}
+      className={classNames('h-14 md:h-[60px] lg:h-16 rounded-xl dark:bg-slate-900 outline-0 bg-search bg-no-repeat pl-16 dark:text-slate-100 text-slate-900 bg-[#CFE3FC]', {
+        "border dark:border-sky-200 border-sky-800": isFocused
+      }, [className])}
+      style={{backgroundPosition: '24px'}}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      {...otherProps}
+    />
+  );
+});
+
+export default Input;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:
+        {
+          'search': "url('./src/shared/assets/search.svg')"
+        },
+    },
   },
   plugins: [],
 }
