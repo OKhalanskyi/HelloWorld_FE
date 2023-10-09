5c590afede6d2f3c64f5b6dce4caddbddb23c197
Khalanskyi Oleh <112695863+OKhalanskyi@users.noreply.github.com>
2023-10-09 14:17:16 +0300

    feat: fetch client created

---
+++ src/shared/api/fetchClient.ts
@@ -0,0 +1,33 @@
+import { BASE_URL } from '@/shared/constants/api.ts';
+
+type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
+function request<T>(
+  url: string,
+  method: RequestMethod = 'GET',
+  /* eslint-disable */
+  data: any = null,
+): Promise<T> {
+  const options: RequestInit = { method };
+
+  if (data) {
+    options.body = JSON.stringify(data);
+    options.headers = {
+      'Content-Type': 'application/json; charset=UTF-8',
+    };
+  }
+
+  return fetch(BASE_URL + url, options)
+    .then(() => fetch(BASE_URL + url, options))
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      return response.json();
+    });
+}
+
+export const client = {
+  get: <T>(url: string) => request<T>(url),
+};
